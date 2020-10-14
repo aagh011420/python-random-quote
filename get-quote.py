84c06bc57ae0626e3e91c071346de1c70bd4cf39
aagh011420 <wdldmv110119@homemail.com>
2020-10-14 03:05:33 -0700

    Done for now!

--- get-quote.py
+++ get-quote.py
@@ -8,9 +8,12 @@ def primary():
   quotes = f.readlines()
   f.close()
 
-  last = len(quotes)-1
-  rnd = random.randint(0,last)
-  print(quotes[rnd])
+  #last = len(quotes)-1
+  #rnd = random.randint(0,last)
+  #print(quotes[rnd])
+  for l in range(2):   # dump two quotes
+    print('"' +  quotes[ random.randint( 0, len(quotes)-1 ) ].rstrip('\n') + '"', end='\n')
+
 
 if __name__== "__main__":
   primary()
